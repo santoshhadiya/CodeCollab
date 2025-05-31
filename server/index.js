0e9a124cf027830bf0b35d431b4112b1fc420dc2
Santosh Hadiya <santoshhadiya033@gmail.com>
2025-05-31 16:10:15 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const usersInRoom = {};
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["https://codecollab-frontend-kgxh.onrender.com", "http://localhost:5173"],
     methods: ["GET", "POST"],
   },
 });
